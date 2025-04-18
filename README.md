5804f3a9fb038cfab622ee2c36106708c4a3c875
Roberts Slisans <rsxdalv@gmail.com>
2025-04-18 17:54:36 +0300

    Split from main repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Extension adapter for Maha TTS
+
+License - the source code within this repository is licensed under the MIT license.
+However, when installing this package, Maha TTS license takes precedence and the final license is Apache 2.0.
+So, for most intents and purposes, the Apache 2.0 license is the one that applies.
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="extension_maha_tts",
+    packages=setuptools.find_namespace_packages(),
+    version="0.0.1",
+    author="rsxdalv",
+    description="Maha TTS allows generating speech from text using the MahaTTS model.",
+    url="https://github.com/rsxdalv/extension_maha_tts",
+    project_urls={},
+    scripts=[],
+    install_requires=[
+        "maha-tts @ https://github.com/rsxdalv/MahaTTS/releases/download/v1.0.0/maha_tts-1.0.0-py3-none-any.whl",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
